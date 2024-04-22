b52aef482ecdb4025dde1e11823258f9750cf5fb
hyperLink-19 <linhnguyen.dev@outlook.com>
2024-04-22 22:32:22 +0700

    add function to count number of connection to mongoDB

--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const mongoose = require('mongoose')
-
 const connectString = `mongodb://localhost:27017/myshop`
+const { countConnect } = require('../helpers/check.connect')
 
 class Database {
   constructor() {
@@ -16,7 +16,7 @@ class Database {
     }
     mongoose
       .connect(connectString)
-      .then(() => console.log('Connected to MongoDB'))
+      .then(() => console.log('Connected to MongoDB!', countConnect()))
       .catch((err) => console.log('Connect to MongoDB Error:', err))
   }
 
